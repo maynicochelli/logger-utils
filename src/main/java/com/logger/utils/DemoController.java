1bc7626999d743b35671abf5f730b9dc138d1293
Diego Gean da Fé <diegogeandafe@gmail.com>
2022-03-27 15:36:45 -0300

    Update log

--- src/main/java/com/logger/utils/DemoController.java
+++ src/main/java/com/logger/utils/DemoController.java
@@ -1,7 +1,5 @@
 package com.logger.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +9,6 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/teste")
 public class DemoController {
-
-    private static final Logger logger = LoggerFactory
-            .getLogger(DemoController.class);
 
     @GetMapping
     public void getMessage() throws IOException, NoSuchFieldException {
